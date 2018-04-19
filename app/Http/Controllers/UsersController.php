5340f23e9acf6ef1cd157c839645b8084b1095b1
vagrant <vagrant@homestead>
2018-04-19 07:42:27 +0000

    Fix bug

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -50,30 +50,30 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
-        $this->authorize('update',$user);
-        return view('users.edit',compact('user'));
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
     }
 
-    public function update(User $user,Requeset $requeset)
+    public function update(User $user, Request $request)
     {
-        $this->validate($request,[
-            'name'=>'required|max:50',
-            'password'=>'required|confirmed|min:6'
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
         ]);
 
-        $this->authorize('update',$user);
+        $this->authorize('update', $user);
 
-        $data =[];
-        $data['name']=$requeset->name;
-        if($requeset->password){
-            $data['password'] = bcrypt($requeset->password);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
         }
-
+        
         $user->update($data);
 
-        session()->flash('success','个人资料更新成功！');
+        session()->flash('success', '个人资料更新成功！');
 
-        return redirect()->route('users.shwo',$user->id);
+        return redirect()->route('users.show', $user->id);
     }
     public function index()
     {
